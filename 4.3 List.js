57e1932dbae0cb257474aa3e6de576613ddb98fe
Bernadetta Wąsowska <bernadetta.brzeczek@deliveryhero.com>
2018-06-06 09:42:25 +0200

    Update 4.3 List.js

--- 4.3 List.js
+++ 4.3 List.js
@@ -1,41 +1,63 @@
-// Your code here.
-
 function arrayToList(array) {
-  array=array.reverse();
-  
-  let list = null;
-  
-  array.forEach(function(element, i) {
-  	//console.log('Element', i, 'is', element);
-    list = {
-  		value: element,
-  		rest: list
-	};
+    array=array.reverse();
+
+    let list = null;
+
+    array.forEach(function(element, i) {
+        list = {
+          value: element,
+          rest: list
+      };
   });
-  return list;
+    return list;
 }
+
 function arrayToListRecursive(array) {
-  if(array.length===1){
-  	list = {
-  		value: array[0],
-  		rest: null
-	};
+    if(array.length===1){
+        list = {
+          value: array[0],
+          rest: null
+      };
   }else if(array.length>1) {
-      arrayToListRecursive(array.slice(1, array.length))
-      list = {
-  		value: array[0],
-  		rest: list
-	};
-  }
-  return list;
+    arrayToListRecursive(array.slice(1, array.length))
+    list = {
+      value: array[0],
+      rest: list
+  };
+}
+return list;
 }
 
+function listToArray(list, arr) {
+    if(list.rest===null) {
+        arr.push(list.value);
+    } else {
+        listToArray(list.rest, arr);
+        arr.unshift(list.value);
+    }
+    return arr;
+}
 
+function prepend(value, list) {
+    let node = {value: value, rest: list}
+    return node;
+}
+
+function nth(list, number) {
+    array = listToArray(list, []);
+    if (number<0 || number>=array.length) {
+        console.log("Incorrect number");
+        return {};
+    } else {
+        return array[number];
+    }
+
+}
 
 console.log(arrayToList([10, 20, 30, 40]));
 console.log(arrayToListRecursive([10, 20, 30, 40]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(listToArray(arrayToList([10, 20, 30]),[]));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
